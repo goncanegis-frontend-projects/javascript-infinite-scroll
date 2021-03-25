ea745535a033447e1ed6f93b7c9878955ecf83d4
celeis <gonca.negis@gmail.com>
2021-03-25 09:37:11 +0300

    changed initial image count

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 30;
+let count = 5;
 const apiKey = 'BtI7OEzTWiW0AAL1GKWKeAB5-hlSxfbIFOyAHsxxyLE';
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
@@ -17,6 +17,7 @@ function imageLoaded() {
     if (imagesLoaded === totalImages) {
         ready = true;
         loader.hidden = true;
+        count = 30;
     }
 }
 
